8ee31fc992c16a711e19122a2a6903da2882eedb
Alexandra Gesler <alexgesler95@gmail.com>
2025-07-25 18:33:23 +0200

    created the header file and build a first skeleton, added the get_next_line function

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegesle <alegesle@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 17:25:19 by alegesle          #+#    #+#             */
+/*   Updated: 2025/07/25 18:18:42 by alegesle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h> //for read function
+
+char	*get_next_line(int fd);
+
+#endif
