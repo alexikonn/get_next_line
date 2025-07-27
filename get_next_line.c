b85af749a02e471a926ea4d3d6fd81955472b683
Alexandra Gesler <alexgesler95@gmail.com>
2025-07-27 21:49:05 +0200

    added a main to experiment with open and read

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alegesle <alegesle@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:18:04 by alegesle          #+#    #+#             */
-/*   Updated: 2025/07/25 17:22:00 by alegesle         ###   ########.fr       */
+/*   Updated: 2025/07/27 21:46:30 by alegesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,25 @@
 char	*get_next_line(int fd)
 {
 	
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{	
+	int		fd;
+	char	buffer[50];
+	int		count;
+
+	count = 0;
+	fd = open("./freddy.txt", O_RDONLY);
+	while (count < 3)
+	{
+		count++;
+		read(fd, buffer, 10);
+		printf("First 10 bytes: %s\n", buffer);
+	}
+	close(fd);
+	return (0);
 }
