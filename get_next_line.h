13223c7bd2db22f54b9dcc381f76ef721111f80d
Alexandra Gesler <alexgesler95@gmail.com>
2025-08-19 12:05:55 +0200

    added new functions prototype to the header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: alegesle <alegesle@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:25:19 by alegesle          #+#    #+#             */
-/*   Updated: 2025/08/18 23:26:49 by alegesle         ###   ########.fr       */
+/*   Updated: 2025/08/19 11:35:06 by alegesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,14 @@
 # include <stdlib.h> // malloc & free
 
 char	*get_next_line(int fd);
+int		has_new_line(const char *s);
 
 
 #endif
 
 //we learned about macros, how to define, checked with the Norm & why they are in the header file like this
 // the 42 value is by default as subject says
+
+// when BUFFER_SIZE = 0 > First 10 bytes: �
+						// First 10 bytes: ]
+						// First 10 bytes: ]
