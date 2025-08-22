7890f940c34c958d4ea4b9b52bcacd22a71d0c25
Alexandra Gesler <alexgesler95@gmail.com>
2025-08-22 21:39:38 +0200

    deleted ft_strdup & ft_strlcpy and replaced it with ft_strjoin, which is specific for getnextline. started creating a while loop which looks for a new-line-character in line - line = joined strings

--- get_next_line.c
+++ get_next_line.c
@@ -6,50 +6,13 @@
 /*   By: alegesle <alegesle@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:18:04 by alegesle          #+#    #+#             */
-/*   Updated: 2025/08/22 15:14:36 by alegesle         ###   ########.fr       */
+/*   Updated: 2025/08/22 21:36:12 by alegesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	src_len;
-	size_t	j;
-
-	src_len = 0;
-	while (src[src_len])
-		src_len++;
-	if (size == 0)
-		return (src_len);
-	j = 0;
-	while (src[j] != '\0' && j < size -1)
-	{
-		dst[j] = src[j];
-		j++;
-	}
-	dst[j] = '\0';
-	return (src_len);
-}
-// copied function from libft
-//changed variable names to more descriptive names 
-
-char	*ft_strdup(const char *s)
-{
-	char	*copy;
-	size_t	len;
-
-	len = 0;
-	while (s[len] != '\0' && len < BUFFER_SIZE)
-		len++;
-	copy = malloc(len + 1);
-	if (!copy)
-		return (NULL);
-	ft_strlcpy(copy, s, len + 1);
-	return (copy);
-}
 // wrote a while loop for finding out the length of buffer and saving it in the variable len
 // ft_strdup duplicates the buffer and I created buffer_dup, which contains the copied buffer
 // it was nessasry to create a buffer_dup, because buffer is a local variable and is not working outside the function
@@ -100,24 +63,36 @@ char	*ft_strjoin(const char *s1, const char *s2)
 //created this function specialized for getnextline
 //it joins string 1 and string 2 until the null-character
 //learned about the difference between ++i and i++
+//deleted ft_strdup and replaced it with ft_strjoin
 
 char	*get_next_line(int fd)
 {
-	char	buffer[BUFFER_SIZE];
-	char	*buffer_dup;
+	char	buffer[BUFFER_SIZE + 1];
+	char	*line;
 	size_t	i;
 
 	i = 0;
-	while (i < BUFFER_SIZE)
+	while (i < BUFFER_SIZE + 1)
 	{
 		buffer[i] = 0;
 		i++;
 	}
-	read(fd, buffer, BUFFER_SIZE);
-	buffer_dup = ft_strdup(buffer);
-	return (buffer_dup);
+	while (has_new_line(line) == 0)
+	
+
+	
+
+	//read(fd, buffer, BUFFER_SIZE);
+	//line = ft_strjoin(buffer, "");
+	return (line);
 }
+
+//started a while loop, which checks if there is a new line charackter in the line variable,
+//which is the joined string
+
+
 // Buffer_SIZE is the Macro for the size of buffer, which will be added to compile during evaluation
+//line = variable wihich contains the joined strings, if one of the string is an empty string, this is how it is portraited ""
 
 //while compiling with different BUFFER_SIZES we found out, that len of the string should be < BUFFER_SIZE
 
