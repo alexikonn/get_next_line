0ec2f86622fff3d89ab9cd1ec74642ca42948ef3
Alexandra Gesler <alegesle@student.42berlin.de>
2025-09-08 14:08:45 +0200

    deleted comments

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: alegesle <alegesle@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:23:35 by alegesle          #+#    #+#             */
-/*   Updated: 2025/09/01 15:37:33 by alegesle         ###   ########.fr       */
+/*   Updated: 2025/09/08 12:46:20 by alegesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,9 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
-char	*build_line(char **line_ptr) //THISSSSW!!!!! function I took out of get_next_line,  this function is used when new-line-character is found
-{	
-	size_t	sub_len; //PUT THIS FUNCTION IN UTILS FIRST; BECAUSE IT'S THE MOST IMPORTANT & BEFORE THAT THE PROTOTYPES OF THE FUNCTIONS THAT ARE usED BUT NOT IN THE HEADER
-	//length of the substring
-	 ////line is initialized to an empty string and then line becomes the new string through ft_strjoin (joins line and buffer)
+char	*build_line(char **line_ptr)
+{
+	size_t	sub_len;
 	char	*free_line;
 	char	*sub_line;
 	char	*line;
@@ -27,20 +25,18 @@ char	*build_line(char **line_ptr) //THISSSSW!!!!! function I took out of get_nex
 	line = *line_ptr;
 	if (ft_strchr(line, '\n') != NULL)
 	{
-		sub_len = ft_strchr(line, '\n') - line +1; //sub_len is as long as the pointer to line until new line character is found 
-	//(f.ex. first character of a string in BUFFER_SIZE) minus pointer to current line + 1 for the new line character
-		sub_line = ft_substr(line, 0, sub_len); // sub_line is the new string which is build out of a line until a newline character (including it)
+		sub_len = ft_strchr(line, '\n') - line + 1;
+		sub_line = ft_substr(line, 0, sub_len);
 		free_line = line;
 		*line_ptr = ft_substr(line, sub_len, BUFFER_SIZE);
-		//line saves the substr which is the rest from sub_line and as line is a static variable, in the next call we start with the saved part
 		free(free_line);
-	}//check if line is an empty string, so it returns NULL, because NULL [i] -> segfault. i can´t iterate through a null pointer
+	}
 	else
-	{	
+	{
 		sub_line = line;
 		*line_ptr = NULL;
 	}
-	return (sub_line); //returns the new line (sub_line) including the new line character (compiled, worked)
+	return (sub_line);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -49,7 +45,7 @@ char	*ft_strchr(const char *s, int c)
 
 	i = 0;
 	if (s == NULL)
-		return(NULL);
+		return (NULL);
 	while (s[i] != '\0')
 	{
 		if (s[i] == (char)c)
@@ -61,11 +57,8 @@ char	*ft_strchr(const char *s, int c)
 	else
 		return (NULL);
 }
-//searches for the new line character in my string line
-//if it is found, returns a pointer to that part in memory where it is found
-//if it is not found, returns NULL
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size) //STATIC FUNCTION
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
@@ -85,15 +78,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size) //STATIC FUNCTION
 	return (i);
 }
 
-// wrote a while loop for finding out the length of buffer and saving it in the variable len
-// ft_strdup duplicates the buffer and I created buffer_dup, which contains the copied buffer
-// it was nessasry to create a buffer_dup, because buffer is a local variable and is not working outside the function
-//added len < BUFFER_SIZE to the while loop, which makes rue that only as many carachters are copied as the BUFFER_SIZE. 
-// -> before that, while printing there were more characters than BUFFER_SIZE, because len was always searching for '\0'
-
-//WHAT happens with ft_strjoin when one s is NULL, handle like empty strings??
 char	*ft_strjoin(const char *s1, const char *s2)
-{ 
+{
 	char	*result;
 	int		len1;
 	int		len2;
@@ -105,14 +91,10 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	len2 = 0;
 	while (s2 != NULL && s2[len2] != '\0')
 		len2++;
-	// find out length of s1 and s2
 	result = malloc(len1 + len2 + 1);
-	//allocate memory as much as sum of s1+s2 & one more for the null terminator
 	if (!result)
 		return (NULL);
-	//checks if condition is true, so result is NULL (result == 0), it returns NULL immediately 
 	result[len1 + len2] = '\0';
-	//null-terminating the result string
 	i = -1;
 	while (s1 != NULL && s1[++i] != '\0')
 		result[i] = s1[i];
@@ -120,17 +102,9 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	while (s2 != NULL && s2[++i] != '\0')
 		result [i + len1] = s2[i];
 	return (result);
-	//starting with i = -1, because we increase i in the while conditions first, so that means s[i] goes to 1 and does not work with the char on the 0 positions
 }
-//created this function specialized for getnextline
-//it joins string 1 and string 2 until the null-character
-//learned about the difference between ++i and i++
-//str_dup is not nessessary, because strjoin can also a join a string with an empty string
 
-//function copies only max of size characters of a string src into dst,
-//it null-terminates the dst and returns the length of the full string s1
-
-char	*ft_substr(char const *s, unsigned int start, size_t len) //STATIC FUNCTION; NOT IN HEADER
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	s_len;
@@ -150,8 +124,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len) //STATIC FUNCTION
 	ft_strlcpy(sub, s + start, sub_len + 1);
 	return (sub);
 }
-
-//function creates a substring out of the string s beginning at the start point start
-//the length of the substring is max len
-
-
