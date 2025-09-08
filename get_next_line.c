081c62872c90bf9877f79f24c22ff38cee32eb0f
Alexandra Gesler <alegesle@student.42berlin.de>
2025-09-08 14:08:25 +0200

    deleted comments

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,34 @@
 /*   By: alegesle <alegesle@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:18:04 by alegesle          #+#    #+#             */
-/*   Updated: 2025/09/01 15:33:52 by alegesle         ###   ########.fr       */
+/*   Updated: 2025/09/08 14:00:46 by alegesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-
+//as long as \n is not found, 
 char	*get_next_line(int fd)
 {
 	char		buffer[BUFFER_SIZE + 1];
 	static char	*line = NULL;
-	//static variable allocates memory, its value is the rest which comes after the new-line-character - LEARN MORE ABOUT StatIC VARIABLES
 	size_t		i;
 	char		*sub_line;
-	//the new string which is the subline until the new line character
 	char		*free_line;
 
 	while (ft_strchr(line, '\n') == NULL)
-	//as long as new line character is not found
 	{
 		i = 0;
 		while (i < BUFFER_SIZE + 1)
 		{
 			buffer[i] = 0;
 			i++;
-		} //every i in buffer is initialized to zero, to make sure everything inside is overwrite by zero
-		if (read(fd, buffer, BUFFER_SIZE) == 0) //CHECK EDGDE CASE IF LAST CHARACTER IN FILE IS NEW LINE CHARACHTER
-			break ;//read only read as long as the end of file is reached, when this happens, there is a function break
+		}
+		if (read(fd, buffer, BUFFER_SIZE) == 0)
+			break ;
 		free_line = line;
 		line = ft_strjoin(line, buffer);
 		free(free_line);
-		//line is always the joined string starts at the first char which is in the text file and joins everything that is read in BUFFER_SIZE
 	}
 	sub_line = build_line(&line);
 	return (sub_line);
