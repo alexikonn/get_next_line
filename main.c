0a6cd9c56b9e8dd5a7c3bd7ff48170d71aaa4f56
Alexandra Gesler <alexgesler95@gmail.com>
2025-10-05 23:06:45 +0200

    added printf statements to see what fails

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_get_next_line.c                               :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alegesle <alegesle@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 11:29:04 by alegesle          #+#    #+#             */
-/*   Updated: 2025/09/25 11:50:13 by alegesle         ###   ########.fr       */
+/*   Updated: 2025/10/05 21:04:09 by alegesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@
 int	main(void)
 {
 	int		fd;
-	char	*line;
+	//char	*line;
 
-	fd = open("./file.txt", O_RDONLY);
+	fd = open("./nl", O_RDONLY);
 	//fd = -1;
 	printf("%s", get_next_line(fd));
+	printf("hallo|%s|", get_next_line(fd));
+	printf("huhu%s", get_next_line(fd));
+	printf("ja|%s|", get_next_line(fd));
 	/*printf("%d\n", fd);
 	if (fd < 0)
 	{
@@ -31,14 +34,14 @@ int	main(void)
 	//printf("\nBUFFER_SIZE: %d\n\n", BUFFER_SIZE);
 	
 	
-	line = get_next_line(fd);
+	/*line = get_next_line(fd);
 	while (line != NULL)
 	{
 		printf("%s", line);
 		free(line);
 		line = get_next_line(fd);
 	}
-	//printf("\n");
+	//printf("\n"); */
 
 	close(fd);
 	return (0);
